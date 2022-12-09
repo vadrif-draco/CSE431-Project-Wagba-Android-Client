d38baee42ccba963ccf65f7c88f907c1a975700a
Ahmad S. Salama <vadrif.draco@gmail.com>
2022-12-10 01:29:36 +0200

    Initial Commit

---
+++ app/src/main/java/asu/foe/wagba8805/MainActivity.java
@@ -0,0 +1,14 @@
+package asu.foe.wagba8805;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
