611459a04b311f359d388c6d01198a6a08ae502a
Ahmad S. Salama <vadrif.draco@gmail.com>
2022-12-10 18:51:33 +0200

    Add dummy registration for personal email + A few small tweaks and clean-ups

--- app/src/main/java/asu/foe/wagba8805/PersonalEmailLoginActivity.java
+++ app/src/main/java/asu/foe/wagba8805/PersonalEmailLoginActivity.java
@@ -3,14 +3,25 @@ package asu.foe.wagba8805;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import asu.foe.wagba8805.databinding.PersonalEmailLoginBinding;
 
 public class PersonalEmailLoginActivity extends AppCompatActivity {
+
+  PersonalEmailLoginBinding pelBinding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-    setContentView(R.layout.personal_email_login);
+
+    pelBinding = PersonalEmailLoginBinding.inflate(getLayoutInflater());
+    setContentView(pelBinding.getRoot());
+
+    pelBinding.registerHyperlink.setOnClickListener(v -> {
+      Toast.makeText(this, "Registration is not allowed yet", Toast.LENGTH_SHORT).show();
+    });
   }
 
   @Override
